c258d0be5b3f13d485af507c5bde8befdc4c74d6
JoseBulgarelli <j.bulgarelli.1@estudiantec.cr>
2026-06-11 17:32:11 -0600

    Add files via upload
    90 EN LA REVISION DE ARQUI, LETS FUCKING GO... Ahora a continuar con este...

--- ProyectoPOO/src/logica/Tipo.java
+++ ProyectoPOO/src/logica/Tipo.java
@@ -7,15 +7,17 @@ public class Tipo {
 	private ArrayList<Item> items;
 	
 	public Tipo(String nombre) {
-		
+		this.nombre = nombre;
 	}
 	
 	public void modificarTipo(String nombre, ArrayList<Item> items) {
-		
+		this.nombre = nombre;
+		this.items = items;
 	}
 	
-	public void consultarTipo() {
-		
+	public String consultarTipo() {
+		return "Nombre del tipo: " + nombre +
+				"\nItems de este tipo: " + items;
 	}
 	
 	public String getNombre() {
@@ -23,19 +25,18 @@ public class Tipo {
 	}
 	
 	public void setNombre(String nombre) {
-		
+		this.nombre = nombre;
 	}
 	
 	public ArrayList<Item> getItems() {
-		
+		return items;
 	}
 	
 	public void agregarItem(Item item) {
-		
+		items.add(item);
 	}
 	
 	public void eliminarItem(Item item) {
-		
+		items.remove(item);
 	}
-	
 }
